879b302f69f727e98320edb16e026f424e647fe1
Andrey Larikov <larikov174@gmail.com>
2022-06-08 15:58:00 +0500

    feat: add styles to news component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,23 +1,59 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import useFetch from '../hooks/useFetch';
+import rssImg from '../assets/rss.svg';
+
+const Container = styled.div`
+	width: 100%;
+	height: auto;
+	padding: 0 20px;
+`;
+
+const StyledListItem = styled.li`
+	display: grid;
+	grid-template-columns: 30px repeat(2, 1fr) 100px;
+	grid-template-rows: repeat(2, 1fr);
+	grid-template-areas:
+		'image title author date'
+		'image score ... date';
+`;
+
+const StyledText = styled.p`
+	color: ${(props) => props.color || '#7d7d7d'};
+	grid-area: ${(props) => props.area};
+`;
+
+const StyledImage = styled.span`
+	background-image: url(${(props) => props.image});
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center;
+	width: 15px;
+	height: 15px;
+	display: block;
+	grid-area: image;
+`;
 
 const News = () => {
-	const {posts, loading, error} = useFetch();
-  if (loading) return <div>Loading...</div>;
-	if (error) return <div>Error occurred, try again later...</div>
-
+	const { posts, loading, error } = useFetch();
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>Error occurred, try again later...</div>;
 
 	return (
-    <div>
-      <h1>HackerNews Top 10 Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>
-            <a href={post.url}>{post.title}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+		<Container>
+			<ul>
+				{posts.map((post) => (
+					<StyledListItem key={post.id}>
+						<StyledImage image={rssImg} />
+						<StyledText color='#000000' area='title'>
+							{post.title}
+						</StyledText>
+						<StyledText area='author'>by {post.by}</StyledText>
+						<StyledText area='score'>{post.score} points</StyledText>
+						<StyledText area='date'>{post.time}</StyledText>
+					</StyledListItem>
+				))}
+			</ul>
+		</Container>
 	);
 };
 
