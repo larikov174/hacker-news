dafe8e0be2441728533c5b94ac95baf2bb064ca9
Andrey Larikov <larikov174@gmail.com>
2022-06-14 02:33:33 +0500

    feat: add rtk query data fetch

--- src/app/story/storyApi.js
+++ src/app/story/storyApi.js
@@ -6,25 +6,24 @@ export const storyApi = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
 	tagTypes: ['Stories'],
 	endpoints: (builder) => ({
-		getStories: builder.query({
-			query: (limit = 5) => `/newstories.json?print=pretty&limitToFirst=${limit}&orderBy="$key"`,
-			providesTags: (result) =>
-			result
-				? [...result.map(({ id }) => ({ type: 'Stories', id })), { type: 'Stories', id: 'LIST' }]
-				: [{ type: 'Stories', id: 'LIST' }],
-		}),
-		// getStoryById: builder.query({
-		// 	query: (id) => `/item/${id}.json?print=pretty`,
-		// 	providesTags: ['Story'],
+		// getStories: builder.query({
+		// 	query: (limit = 5) => `/newstories.json?print=pretty&limitToFirst=${limit}&orderBy="$key"`,
+		// 	providesTags: (result) =>
+		// 		result
+		// 			? [...result.map(({ id }) => ({ type: 'Stories', id })), { type: 'Stories', id: 'LIST' }]
+		// 			: [{ type: 'Stories', id: 'LIST' }],
 		// }),
 		getPosts: builder.query({
-			async queryFn(_arg, _queryApi, _extraOptions, fetchWithBQ) {
-				const arrayWithIds = await fetchWithBQ(`/newstories.json?print=pretty&limitToFirst=${_arg}&orderBy="$key"`);
-				if (arrayWithIds.error) throw arrayWithIds.error;
-				const stories = arrayWithIds.data;
-				const promises = stories.map((id) => fetchWithBQ(`/item/${id}.json?print=pretty`));
-				const result = await Promise.all(promises);
-				return await result.data ? { data: result.data } : { error: result.error };
+			queryFn: async (limit = 5, _queryApi, _extraOptions, fetchWithBQ) => {
+				const arrayWithIds = await fetchWithBQ(`/newstories.json?print=pretty&limitToFirst=${limit}&orderBy="$key"`);
+				try {
+					const stories = await arrayWithIds.data;
+					const promises = stories.map((id) => fetchWithBQ(`/item/${id}.json?print=pretty`));
+					const data = await Promise.all(promises);
+					return { data };
+				} catch (e) {
+					return { error: e.message };
+				}
 			},
 			providesTags: (result) =>
 				result
@@ -34,4 +33,4 @@ export const storyApi = createApi({
 	}),
 });
 
-export const { useGetPostsQuery, useGetStoriesQuery } = storyApi;
+export const { useGetPostsQuery } = storyApi;
--- src/components/StoryList.jsx
+++ src/components/StoryList.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import StoryCard from './StoryCard';
-import { useGetPostsQuery, useGetStoriesQuery } from '../app/story/storyApi';
+import { useGetPostsQuery } from '../app/story/storyApi';
 
 const Container = styled.section`
 	width: 100%;
@@ -9,20 +9,14 @@ const Container = styled.section`
 `;
 
 const StoryList = ({ selectedStory }) => {
-	const {data = [], isLoading, isError} = useGetPostsQuery(6);
-	console.log(isLoading, data);
-	const {data:newD} = useGetStoriesQuery();
+	const { data = [], isLoading, isError } = useGetPostsQuery(5);
 	if (isLoading) return <div>Loading...</div>;
 	if (isError) return <div>Error occurred</div>;
 
 	return (
 		<Container>
-			{!isLoading && data.map((post) => (
-				<StoryCard key={post.data.id} story={post.data} selectedStoryId={selectedStory} />
-			))}
-			{newD && newD.map(item=>(
-				<div key={item}>{item}</div>
-			))}
+			{!isLoading &&
+				data.map((post) => <StoryCard key={post.data.id} story={post.data} selectedStoryId={selectedStory} />)}
 		</Container>
 	);
 };
